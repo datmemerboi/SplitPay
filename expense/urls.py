167d4250f4ba641dbcb98ef8feed21efece6d4e6
datmemerboi <shravanssd@gmail.com>
2020-04-24 17:14:46 +0530

    New expense working

--- expense/urls.py
+++ expense/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('new/', views.newExpenseFn, name="New Expense")
+	path('new/', views.newExpenseFn, name="new expense"),
+	path('create/', views.createExpenseFn, name="create expense"),
 ]
--- expense/views.py
+++ expense/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt,csrf_protect
 
-# Create your views here.
 def newExpenseFn(request):
 	return render( request, "new expense.html", {'title': "New Expense"})
+
+@csrf_exempt
+def createExpenseFn(request):
+	csrfContext = RequestContext(request)
+	print( request.method )
+	print( request.POST['name'])
+	print( request.POST.getlist('actors') )
+	return HttpResponse('Success')
